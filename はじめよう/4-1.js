af21165c4aa53cdda8277c10fa5c04a20f842de0
KotohaSaki <kotoha.saki.14@gmail.com>
2023-09-10 20:44:42 +0900

    change

---
+++ はじめよう/4-1.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 100000; i++) {
+    if (i % 3 === 0) {
+        document.write('Fizz ');
+    } else if(i % 5 === 0){
+        document.write('Buzz ');
+    } else  if(){
+        document.write(i + ' ');
+    }
+    
+}
--- はじめよう/4.js
+++ はじめよう/4.js
@@ -1,8 +1,3 @@
-/* for (let i = 1; i <= 9000; i++) {
-    console.log(i);
-    document.write(i + ' ');
-} */
-
 for (let i = 1; i <= 10; i--) {
     console.log(i);
 }
